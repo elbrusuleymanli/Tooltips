3a681ed1afef08d75d889c286c4ecdd05723833a
elbrusuleymanli <elbrusis@code.edu.az>
2021-01-20 23:41:06 +0400

    tooltips

---
+++ assets/js/main.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+    
+   
+  $(".top").mouseleave(function(){
+    $(".top").children().hide();
+   
+  });
+
+    $(".top").mouseenter(function(){
+    $(".top").children().css({"color":"#fff"}).show();
+   
+  });
+  
+  $(".left").hover(function(){
+    $(".left").children().hide();
+   
+  });
+
+    $(".left").mouseover(function(){
+      $(".left").children().show();
+   
+  });
+
+   $(".right").hover(function(){
+    $(".right").children().hide();
+   
+
+  });
+
+    $(".right").mouseover(function(){
+      $(".right").children().show();
+    
+  });
+  $(".bottom").hover(function(){
+    $(".bottom").children().hide();
+   
+
+  });
+
+    $(".bottom").mouseover(function(){
+      $(".bottom").children().show();
+   
+  });
+  
+   
+     
+    
+   
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    });
